51af27eb10bd6d4fff64ad0007a1555ebea07baf
gagreene <map.n.trowel@gmail.com>
2025-11-06 11:26:12 -0800

    Revised code to assign invalid fuel types as non-fuel (19);

--- cffbps.py
+++ cffbps.py
@@ -398,7 +398,7 @@ class FBP:
         # Apply an additional mask to remove fuel types that are not in the LUT dictionary
         valid_fuel_types = fbpFTCode_NumToAlpha_LUT.keys()  # Get valid numeric codes
         invalid_mask = ~np.isin(self.fuel_type, list(valid_fuel_types))
-        self.fuel_type = mask.array(self.fuel_type, mask=invalid_mask | self.fuel_type.mask)
+        self.fuel_type = mask.where(invalid_mask, 19, self.fuel_type)
 
         # Get unique fuel types present in the dataset
         self.ftypes = [ftype for ftype in np.unique(self.fuel_type) if ftype in list(self.rosParams.keys())]
